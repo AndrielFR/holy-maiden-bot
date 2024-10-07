d192bdda40eb80c6a45c191d70e2646c90066447
AndrielFR <andrielfr@proton.me>
2024-10-06 22:18:07 -0300

    feat(char): add aliases to `/char`
    - /c
    - /perso
    - /personagem
    - /p

--- src/routers/admin/character.rs
+++ src/routers/admin/character.rs
@@ -29,7 +29,12 @@ pub fn router() -> Router {
         ))
         .add_handler(Handler::new_message(
             see_character,
-            macros::command!("char").or(macros::command!("character")),
+            macros::command!("char")
+                .or(macros::command!("character"))
+                .or(macros::command!("c"))
+                .or(macros::command!("perso"))
+                .or(macros::command!("personagem"))
+                .or(macros::command!("p")),
         ))
 }
 
