0c5b0e328238ea2d1f3539b8f397922169c6c65f
AndrielFR <andrielfr@proton.me>
2024-10-13 10:08:09 -0300

    fix(send_character): check character' scape before all
    - avoid character escaping much later

--- src/middlewares/send_character.rs
+++ src/middlewares/send_character.rs
@@ -48,6 +48,7 @@ impl MiddlewareImpl for SendCharacter {
 
         if let Some(message) = message {
             if let Some(Chat::Group(group)) = chat {
+                let conn = db.get_conn();
                 let group_id = group.id();
 
                 let (num_messages, num_needed) = self.chats.entry(group_id).or_insert((
@@ -56,50 +57,47 @@ impl MiddlewareImpl for SendCharacter {
                 ));
                 *num_messages += 1;
 
-                if num_messages >= num_needed {
-                    *num_messages = 0;
-                    *num_needed = thread_rng().gen_range(self.min_messages..self.max_messages);
+                let last_group_character =
+                    GroupCharacter::select_last_by_id(conn, group_id).await?;
 
-                    let conn = db.get_conn();
+                if let Some(ref group_character) = last_group_character {
+                    if group_character.available {
+                        // Check if the character is left behind without anyone collecting it
+                        if (message.id() - group_character.last_message_id) >= 35 {
+                            if let Some(character) =
+                                Character::select_by_id(conn, group_character.character_id).await?
+                            {
+                                // Reset message count
+                                *num_messages = 0;
 
-                    let last_group_character =
-                        GroupCharacter::select_last_by_id(conn, group_id).await?;
+                                // Delete group last character
+                                GroupCharacter::delete_by_id(
+                                    conn,
+                                    group_character.group_id,
+                                    group_character.character_id,
+                                )
+                                .await?;
 
-                    if let Some(ref group_character) = last_group_character {
-                        if group_character.available {
-                            // Check if the character is left behind without anyone collecting it
-                            if (message.id() - group_character.last_message_id) >= 35 {
-                                if let Some(character) =
-                                    Character::select_by_id(conn, group_character.character_id)
-                                        .await?
-                                {
-                                    // Reset message count
-                                    *num_messages = 0;
-
-                                    // Delete group last character
-                                    GroupCharacter::delete_by_id(
-                                        conn,
-                                        group_character.group_id,
-                                        group_character.character_id,
+                                // Send the reply message
+                                message
+                                    .respond(
+                                        InputMessage::html(
+                                            t("character_escaped")
+                                                .replace("{name}", &character.name),
+                                        )
+                                        .reply_to(Some(group_character.last_message_id)),
                                     )
                                     .await?;
 
-                                    // Send the reply message
-                                    message
-                                        .respond(
-                                            InputMessage::html(
-                                                t("character_escaped")
-                                                    .replace("{name}", &character.name),
-                                            )
-                                            .reply_to(Some(group_character.last_message_id)),
-                                        )
-                                        .await?;
-
-                                    return Ok(());
-                                }
+                                return Ok(());
                             }
                         }
                     }
+                }
+
+                if num_messages >= num_needed {
+                    *num_messages = 0;
+                    *num_needed = thread_rng().gen_range(self.min_messages..self.max_messages);
 
                     if let Some(random_character) = Character::select_random(conn).await? {
                         // If the character is the last one, skip it
